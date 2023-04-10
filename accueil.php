040d05576a7d6927d85aab0b0163fbecbab6c6a4
OLAITAN <adoukeola1@gmail.com>
2023-04-10 12:44:08 +0200

    Modification de l'accueil

---
+++ accueil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ESGIS FOOD - Accueil</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Accueil</a></li>
+				<li><a href="#">Menu</a></li>
+				<li><a href="#">Commande en ligne</a></li>
+				<li><a href="#">A propos</a></li>
+				<li><a href="#">Contact</a></li>
+				<li style="float:right"><a href="#">S'inscrire</a></li>
+				<li style="float:right"><a href="#">Se connecter</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<h1>ESGIS FOOD</h1>
+		<p>Bienvenue sur le site de notre restaurant spécialisé dans la vente des mets. Découvrez notre menu et commandez en ligne. </p>
+		<button>Commandez maintenant</button>
+	</main>
+
+	<footer>
+		<p>ESGIS FOOD - Tous droits réservés &copy; 2023</p>
+	</footer>
+</body>
+</html>
